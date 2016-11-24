59b1609cb0e30b3fd6df6dce9090579b8ce01722
David Maitland <davidmaitland777@gmail.com>
2016-11-24 18:07:28 +1000

    + Credits screen + Credit and Quit buttons on title screen functional + Menu button in credits works

---
+++ Assets/quit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quit : MonoBehaviour {
+
+	// Use this for initialization
+	public void quitapplication()
+    {
+        Debug.Log("Quitted");
+        Application.Quit();
+    }
+}
---
+++ Assets/scene_manager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class scene_manager : MonoBehaviour {
+
+	// Use this for initialization
+	public void creditsscene()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void titlescreen()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
