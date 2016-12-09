ed65d052772725ee711369ca4b9196b6d765b0e0
David Maitland <davidmaitland777@gmail.com>
2016-12-09 15:39:52 +1000

    + Buttons for abilities, script to control fire circle ability

---
+++ Assets/Scripts/FireCircle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireCircle : MonoBehaviour
+{
+    public bool IsSelected = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsSelected == true)
+        {
+            OnSelected();
+        }
+
+    }
+    void OnSelected()
+    {
+        Camera camera = GetComponent<Camera>();
+        Vector3 p = camera.ScreenToWorldPoint(new Vector3(100, 100, camera.nearClipPlane));
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(p, 0.1F);
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
